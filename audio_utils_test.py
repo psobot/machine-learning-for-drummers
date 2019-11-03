770cc95c44d95694f436d0f0cc0fa9389338c7bb
Kyle Robinson <krobins5@lakeheadu.ca>
2019-11-03 11:25:28 +0100

    Fixed the first test.

--- audio_utils_test.py
+++ audio_utils_test.py
@@ -4,4 +4,4 @@ import audio_utils
 
 def test_root_mean_square():
     sine_wave = np.sin(np.linspace(-np.pi, np.pi, 201))
-    assert audio_utils.root_mean_square(sine_wave) == 0
+    assert audio_utils.root_mean_square(sine_wave) == 0.7053456158585982
