5404421d5e15304dbb78d16b35d1f5c3dbf34627
Peter Sobot <psobot@gmail.com>
2019-11-03 11:00:50 +0100

    WORKSHOP

---
+++ audio_utils_test.py
@@ -0,0 +1,7 @@
+import numpy as np
+import audio_utils
+
+
+def test_root_mean_square():
+    sine_wave = np.sin(np.linspace(-np.pi, np.pi, 201))
+    assert audio_utils.root_mean_square(sine_wave) == 0
