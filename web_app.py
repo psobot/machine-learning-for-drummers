3c9642539ba4d0eebce6f1256e0fe90adc85b236
Peter Sobot <psobot@gmail.com>
2019-11-03 13:24:23 +0100

    web app

---
+++ web_app.py
@@ -0,0 +1,51 @@
+import os
+from flask import Flask, flash, request, redirect, url_for
+from feature_extract import features_for
+
+
+UPLOAD_FOLDER = './uploads'
+ALLOWED_EXTENSIONS = {'mp3', 'mp4', 'wav', 'aiff'}
+
+app = Flask(__name__)
+app.config['UPLOAD_FOLDER'] = UPLOAD_FOLDER
+
+
+def allowed_file(filename):
+    return '.' in filename and \
+           filename.rsplit('.', 1)[1].lower() in ALLOWED_EXTENSIONS
+
+
+def handle_file_upload(file):
+    print("File uploaded: %s" % file)
+    return repr(features_for(file))
+
+
+@app.route('/', methods=['GET', 'POST'])
+def upload_file():
+    if request.method == 'POST':
+        # check if the post request has the file part
+        if 'file' not in request.files:
+            flash('No file part')
+            return redirect(request.url)
+        file = request.files['file']
+        # if user does not select file, browser also
+        # submit an empty part without filename
+        if file.filename == '':
+            flash('No selected file')
+            return redirect(request.url)
+        if file and allowed_file(file.filename):
+            return handle_file_upload(file)
+
+    return '''
+    <!doctype html>
+    <title>Upload a File</title>
+    <h1>Upload a File</h1>
+    <form method=post enctype=multipart/form-data>
+      <input type=file name=file>
+      <input type=submit value=Upload>
+    </form>
+    '''
+
+
+if __name__ == "__main__":
+    app.run(debug=True)
